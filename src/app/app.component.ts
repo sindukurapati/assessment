91baf498a3b66a585458835204d7aa20c11a0653
sindukurapati <sindukurapati08@gmail.com>
2020-06-21 09:09:03 +0530

    Add dropdown to right side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }]
 })
 export class AppComponent {
   title = 'assessment';
@@ -13,10 +15,20 @@ export class AppComponent {
 
   }
   public tabs = [
-    {'title': 'Tab 1', id:1, active: true},
-    {'title': 'Tab 2', id:2,  active: false},
-    {'title': 'Tab 3', id:3, active: false},
+    { 'title': 'Tab 1', id: 1, active: true },
+    { 'title': 'Tab 2', id: 2, active: false },
+    { 'title': 'Tab 3', id: 3, active: false },
   ];
+  
+
+  public dropdownSettings = {
+    id: "Sample id", 
+    name: "Sample Name", 
+    settings:[
+      {name: 'Save', id: 'saveAction'},
+      {name: 'Edit', id: 'editAction'},
+      {name: 'Delete', id: 'deleteAction'}
+    ] };
 
   public changeActiveTab() {
     this.tabs.forEach(tab => tab.active = false);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HeaderComponent } from './header/header.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { NgxEchartsModule } from 'ngx-echarts';
@@ -23,6 +24,7 @@ import { DemoChartComponent } from './demo-chart/demo-chart.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     TabsModule.forRoot(),
+    BsDropdownModule.forRoot(),    
     NgxEchartsModule.forRoot({
       echarts,
     }),
